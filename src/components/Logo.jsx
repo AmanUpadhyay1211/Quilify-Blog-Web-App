d906ad59a1bc3a8fd4a197411de40def68290d57
Aman Upadhyay <amanupadhyay1211@gmail.com>
2024-07-17 19:14:01 +0530

    move images folder to public directory

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 function Logo({ height = 'h-12', className = '' }) {
   return (
     <div className={`flex items-center ${className}`}>
-      <img src="src/images/QuilifyLogo.png" alt="logo" className={`${height}`} />
+      <img src="/images/QuilifyLogo.png" alt="logo" className={`${height}`} />
     </div>
   );
 }
